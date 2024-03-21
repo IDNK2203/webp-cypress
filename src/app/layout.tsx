d8fb0f253b6afc0260d67d79a721fb12e092d87a
IDNK2203 <efeidukpaye@gmail.com>
2024-03-21 22:16:52 +0100

    setup for shadcn and theming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import db from "@/lib/supabase/db";
+import ThemeProvider from "@/lib/themeProvider/next-theme-provider";
 // import objy from "../../dizzleConfig";
 // db
 
@@ -22,7 +23,11 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
