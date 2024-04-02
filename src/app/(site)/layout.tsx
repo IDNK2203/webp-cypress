b495243a032b26222aa7f18588ae01c6980cf684
IDNK2203 <efeidukpaye@gmail.com>
2024-04-02 12:05:12 +0100

    header Nav Bar UI

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,9 +1,16 @@
+import Header from "@/components/landingPage/header";
+
 function HomePageLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main className="bg-background">{children}</main>;
+  return (
+    <main className="bg-background">
+      <Header />
+      {children}
+    </main>
+  );
 }
 
 export default HomePageLayout;
